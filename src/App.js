c12647e82eee55e03784dd9376e9188a5e940825
Emily Fang <40726681+ef1301@users.noreply.github.com>
2021-03-16 14:12:55 -0400

    SearchBar w/o proper redirecting functionality yet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import SearchBar from './components/functionalComponents/SearchBar';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {Home, Search} from './components/views';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <SearchBar/>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/search/' component={Search}/>
+          </Switch>
+      </Router>
     </div>
   );
 }
