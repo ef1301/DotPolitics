82b814a366ef2d3047851dd3f617addd97a41fd3
ChengDong Ruan <cd54307603@gmail.com>
2021-04-06 14:18:25 -0400

    new way of header

--- src/App.js
+++ src/App.js
@@ -3,19 +3,25 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import {Home, Search} from './components/views';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import RepByLocation from './components/functionalComponents/repByLocation';
+import Navbar from './components/Navbar';
+
+
 
 function App() {
   return (
     <div className="App">
+      
       <Router>
+      <Navbar />
         <SearchBar/>
-          <Switch>
+          <Switch>   
             <Route exact path='/' component={Home} />
             <Route exact path='/search/' component={Search}/>
             <Route exact path='/search/Representatives/:query' children={(props) => <RepByLocation {...props}/>}/>
             <Route exact path='/search/Polls/:query' children={(props) => <RepByLocation {...props}/>}/>
-          </Switch>
+          </Switch>    
       </Router>
+      
     </div>
   );
 }
