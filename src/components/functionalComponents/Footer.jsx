7c7428bbe4b92b1bdccce7cd845e94cb7250a970
Emily Fang <40726681+ef1301@users.noreply.github.com>
2021-04-11 17:49:22 -0400

    Revert "Aboutpage"

--- src/components/functionalComponents/Footer.jsx
+++ src/components/functionalComponents/Footer.jsx
@@ -1,10 +1,10 @@
 import '../styles/FuncComps.css';
-import logo from '../assets/logo_white.png';
+import logo from '../assets/logo.png';
 
 const Footer = () => {
     return (
         <footer>
-            <h1 id="logo"><img src={logo} alt="DotPolitics Logo"></img></h1>
+            <h1 id="logo"><img src={logo} alt="DotPolitics Logo"></img>DotPolitics</h1>
             <div id="footer-content"><h1><a href="https://forms.gle/XM1m6oiSAnDP8BMx9">Contact Us</a></h1></div>
         </footer>
     );
--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -1,8 +1,5 @@
 import SearchBar from '../functionalComponents/SearchBar';
 import '../styles/HomePage.css';
-import {BrowserRouter as Router, Route, Switch,Link} from 'react-router-dom';
-import About from './About';
-
 
 const Home = () => {
     return (
@@ -10,7 +7,11 @@ const Home = () => {
             <SearchBar/>
             <div id="home-content">
                 <div className="home-banner">
-                    
+                    <h1>About</h1>
+                    <p>DotPolitics is a useful tool to look up elected representatives across the United States <b>based on location.</b>
+                    <br></br>Having the means to educate yourself and seek out representative information in a centralized manner is vital for change.
+                    <br></br>We provide basic information and links to each representative's social media(s) and contact information, using the <a href="https://developers.google.com/civic-information/">Google Civics Information API</a>.
+                    </p>
                 </div>
             </div>
             <div id="parallax-1"></div>
