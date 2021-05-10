b551eac533b6d9b3c332e4f97cbb3e6167c410ae
ChengDong Ruan <cd54307603@gmail.com>
2021-05-10 16:14:36 -0400

    upload the new file

---
+++ src/components/functionalComponents/Navbar.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/Navbar.css';
+import logo from '../assets/logo_white.png';
+
+
+function Navbar() {
+    return(
+        <>
+        <nav className='navbar'>   
+            <Link to ='/'
+             className ='navbar-logo'><img id="logo" src={logo} alt="DotPolitics Logo"></img>
+                 DotPolitics
+             </Link>
+             <ul>
+                <li className='nav-item'>
+                    <Link to='/About' className='nav-links' >
+                        About us
+                    </Link>
+                </li>
+            </ul>
+            
+        </nav>
+        </>
+    );
+}
+export default Navbar;
