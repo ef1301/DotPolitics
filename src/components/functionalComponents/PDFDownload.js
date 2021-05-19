2abf13c76cbe2d6586fa427785e7f2bc5ab6f5eb
seasonedtofu <seasonedtofu@gmail.com>
2021-05-19 13:18:55 -0400

    change button to say download results as pdf

--- src/components/functionalComponents/PDFDownload.js
+++ src/components/functionalComponents/PDFDownload.js
@@ -4,7 +4,7 @@ import { PDFDownloadLink} from '@react-pdf/renderer';
 
 const dlPDF = (props) => (
   <div className="pdf" >
-    <PDFDownloadLink document={< PDF {...props} />} fileName="repByLocation.pdf"><button className="pdfbutton">Download now!
+    <PDFDownloadLink document={< PDF {...props} />} fileName="repByLocation.pdf"><button className="pdfbutton">Download Results as PDF!
       {({ blob, url, loading, error }) =>
         loading ? 'Loading document...' : 'Download results as PDF' 
       }</button>
